5703518e0922eff84f99725327265afcf4079d0b
Kevin Sarria <reysabueso256@gmail.com>
2022-04-20 10:55:56 -0500

    Cambios pequeños al js, 20/04/2022

--- js/inicio_admin.js
+++ js/inicio_admin.js
@@ -1,32 +1,22 @@
 'use strict'
-
 let fechas = document.querySelectorAll('.fecha_vencimiento');
 let fechas_lotes = document.querySelectorAll('.fecha_lote');
 let array_fechas = [];
 let array_fechas_lotes = [];
-
 for( let i = 0; i < fechas.length; i++) {
     array_fechas.push(fechas[i]);
     array_fechas_lotes.push(fechas_lotes[i]);
 }
-
 for( let i = 0; i < array_fechas.length; i++) {
-
-
+    let nuevo_lote = array_fechas_lotes[i].innerHTML.split('/', 3);
+    let nueva_fecha_v = array_fechas[i].innerHTML.split('/', 3);
+    // if() {
+    //     array_fechas.forEach(fecha => {
+    //         fecha.style.color = "red";
+    //     });
+    // }
     
-
-    let nuevo_lote = array_fechas_lotes[i].innerHTML.split('/', 2);
-
-    let nueva_fecha_v = array_fechas[i].innerHTML.split('/', 2);
-
-    if() {
-
-        array_fechas.forEach(fecha => {
-            fecha.style.color = "red";
-        });
-
-    }
-
+     
 }
 
 
