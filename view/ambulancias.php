3e365fdbcb9f9adc31379b9aac7e5dd9d11be500
Kevin Sarria <reysabueso256@gmail.com>
2022-06-02 10:38:55 -0500

    Se agregaron las alertas de peso y no vacio para el formulario de registrar ambulancia

--- view/ambulancias.php
+++ view/ambulancias.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -17,26 +17,28 @@ mysqli_fetch_assoc($respuesta);
 
 ?>
 
-    <section class="contenido__principal">
+<section class="contenido__principal">
 
-        <?php foreach($respuesta as $ambulancia): ?>
-        <div class="ambulancia">
-            <a href="/ambulancias-app/view/ver_info_ambulancia.php?id=<?php echo $ambulancia['id'] ?>">
-            <h3>Ambulancia <?php echo $ambulancia['nombre']; ?></h3>
-            <h4>Placa: <?php echo $ambulancia['placa']; ?></h4>
-            <img src="<?php echo $ambulancia['imgaen']; ?>" alt="">
-            </a>
-        </div>
-        <?php endforeach; ?>
-
-        <div class="ambulancia">
-            <a href="/ambulancias-app/view/crear-ambulancia.php">
+    <div class="ambulancia">
+        <a href="/ambulancias-app/view/crear-ambulancia.php">
             <h3>Nueva Ambulancia</h3>
             <img src="/ambulancias-app/img/ambulancia.png" alt="">
+        </a>
+    </div>
+
+    <?php foreach ($respuesta as $ambulancia) : ?>
+        <div class="ambulancia">
+            <a href="/ambulancias-app/view/ver_info_ambulancia.php?id=<?php echo $ambulancia['id'] ?>">
+                <h3>Ambulancia <?php echo $ambulancia['nombre']; ?></h3>
+                <h4>Placa: <?php echo $ambulancia['placa']; ?></h4>
+                <img src="<?php echo $ambulancia['imgaen']; ?>" alt="">
             </a>
         </div>
+    <?php endforeach; ?>
 
-    </section>
+
+
+</section>
 
 
 <?php include('../includes/footer.php'); ?>
--- view/crear-ambulancia.php
+++ view/crear-ambulancia.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(!isset($_SESSION['login'])) {
+if (!isset($_SESSION['login'])) {
     header('location: /ambulancias-app/');
 }
 
@@ -19,32 +19,35 @@ mysqli_fetch_assoc($respuesta);
 
 $errores = [];
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $placa = $_POST['placa'];
     @$tipo_ambulancia = $_POST['tipo_ambulancia'];
     $imagen = md5(uniqid(rand(), true)) . '.jpg';
 
-    if(!$placa) {
+    if (!$placa) {
         $errores[] = "Por favor ingresa la placa de la ambulancia<br>";
     }
 
-    if(!$tipo_ambulancia) {
+    if (!$tipo_ambulancia) {
         $errores[] = "Por favor ingresa el tipo de ambulancia<br>";
     }
 
-    if(!$_FILES['imagen']['name']) {
+    if (!$_FILES['imagen']['name']) {
         $errores[] = "Por favor suba una imagen para reconocer la ambulancia<br>";
     }
 
-    if(empty($errores)) {
+    if ($_FILES['imagen']['size'] >= 1000000) {
+        $errores[] = "La imagen es muy pesada, por favor suba una que pese menos de 1MB<br>";
+    }
+
+
+    if (empty($errores)) {
         move_uploaded_file($_FILES['imagen']['tmp_name'], '../img/' . $imagen);
 
         $conexion = conectarDB();
         $query = "INSERT INTO `ambulancia`(`placa`, `id_tipo_ambulancia`, `imgaen`) VALUES ('$placa','$tipo_ambulancia','/ambulancias-app/img/$imagen')";
         $respuesta = mysqli_query($conexion, $query);
-
     }
-
 }
 
 
@@ -52,24 +55,24 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <section class="registrar-ambulancia">
 
-<?php foreach( $errores as $error ): ?>
-    <div class="alerta error">
-    
-    <?php echo $error; ?>
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
 
-    </div>
+            <?php echo $error; ?>
+
+        </div>
     <?php endforeach; ?>
 
 
     <form method="POST" class="formulario form-crear-ambulancia" enctype="multipart/form-data">
-            
+
         <label for="placa">Placa</label>
         <input type="text" id="placa" name="placa">
 
         <label for="tipo_ambulancia">Tipo de ambulancia</label>
         <select name="tipo_ambulancia" id="tipo_ambulancia">
             <option value="0" disabled selected>-- Seleccione una opción --</option>
-            <?php foreach($respuesta as $r): ?>
+            <?php foreach ($respuesta as $r) : ?>
                 <option value="<?php echo $r['id']; ?>">
                     <?php echo $r['nombre']; ?>
                 </option>
