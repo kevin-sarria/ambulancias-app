bad5e6357c0e8a624bff5a0571378b79208728dd
Kevin Sarria <reysabueso256@gmail.com>
2022-06-21 15:35:07 -0500

    Avances}

--- view/registrar_medicamento.php
+++ view/registrar_medicamento.php
@@ -4,14 +4,23 @@ session_start();
 
 include('../includes/header.php'); 
 include('../model/rutas.php');
+include('../model/conexion.php');
 
 
 if(!isset($_SESSION['login'])) {
     header('location:' . $carpeta_origen);
 }
 
-
 $id_ambulancia = $_GET['id'];
+
+$conexion = conectarDB();
+
+$query_med = "SELECT * FROM medicamentos";
+
+$result_med = mysqli_query($conexion, $query_med);
+
+mysqli_fetch_assoc($result_med)
+
 
 ?>
 
@@ -25,13 +34,15 @@ $id_ambulancia = $_GET['id'];
 
         <label for="name">Nombre</label>
         <select name="name" id="name">
-            <option value="1">Aceptaminofen</option>
-            <option value="2">Paracetamol</option>
-            <option value="3">Tramadol</option>
+            <option disabled selected> -- Seleccione una opcion -- </option>
+            <?php foreach( $result_med as $r ): ?>
+            <option value="<?php echo $r['id'] ?>"><?php echo $r['nombre']; ?></option>
+            <?php endforeach; ?>
         </select>
 
         <label for="tipo_medicamento">Tipo Medicamento</label>
         <select name="tipo_medicamento" id="tipo_medicamento">
+            <option disabled selected> -- Seleccione una opcion -- </option>
             <option value="1">Aceptaminofen</option>
             <option value="2">Paracetamol</option>
             <option value="3">Tramadol</option>
@@ -41,7 +52,7 @@ $id_ambulancia = $_GET['id'];
         <input type="text" id="lote" name="lote">
 
         <label for="fecha_vencimiento">Fecha Vencimiento</label>
-        <input type="text" id="fecha_vencimiento" name="fecha_vencimiento">
+        <input type="date" id="fecha_vencimiento" name="fecha_vencimiento">
 
         <label for="cantidad">Cantidad</label>
         <input type="number" id="cantidad" name="cantidad" min='0'>
